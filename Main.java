4c105b9eb8ffb5a601693b82b9293dfccb3eeb3f
JohnMata0427 <john.mata@epn.edu.ec>
2023-11-24 11:23:22 -0500

    First files

---
+++ Main.java
@@ -0,0 +1,5 @@
+package Class5_of_POO.Class6_of_POO;
+
+public class Main {
+    
+}
---
+++ Students.java
@@ -0,0 +1,8 @@
+package Class5_of_POO.Class6_of_POO;
+
+public class Students {
+    // Attributes
+    String name, lastname, subject;
+    float grades[]; 
+    float average;
+}
