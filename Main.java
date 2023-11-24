c092248b0ea362d3e9aaa838f117d24fa062d37d
jeseniaisabel231 <130105827+jeseniaisabel231@users.noreply.github.com>
2023-11-24 12:52:46 -0500

    last change

--- Main.java
+++ Main.java
@@ -5,11 +5,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Students studentOne;
+        Students studentTwo;
+        Students studentThree;
 
         studentOne = new Students("Carlitos", "Casa");
+        studentTwo = new Students("Isabel", "Paredes");
+        studentThree = new Students("John", "Puerta");
+
         studentOne.inputData();
+        System.out.println("Estudiante: " + studentOne.getName() +" "+ studentOne.getLastname());
         studentOne.printData();
         studentOne.printAverage();
+
+        studentTwo.inputData();
+        System.out.println("Estudiante: " + studentTwo.getName() +" "+ studentTwo.getLastname());
+        studentTwo.printData();
+        studentTwo.printAverage();
+
+        studentThree.inputData();
+        System.out.println("Estudiante: " + studentThree.getName() +" "+ studentThree.getLastname());
+        studentThree.printData();
+        studentThree.printAverage();
     }
     
 }
